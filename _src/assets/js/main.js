62dd5f1210e00a5510bfdf4b3077f6780324cc2c
aroatdlt <aroatato@gmx.com>
2018-12-03 18:29:16 +0100

    Add a counter down

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,7 +4,7 @@ const writeYourNumber = document.querySelector('.choose_number');
 const button = document.querySelector('.btn');
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
 console.log('> ' + getRandomNumber(100));
@@ -14,16 +14,18 @@ const randomNumber = getRandomNumber;
 const getTrack = document.querySelector('.feedback');
 
 const probeYourLuck = () => {
-    
-    if (writeYourNumber.value === randomNumber) {
-        getTrack.innerHTML = `Has ganado CAMPEONA!`;
-    } else if (writeYourNumber.value !== randomNumber) {
-        getTrack.innerHTML = `${writeYourNumber.value} está lejos del número premiado`;
-    }
+
+  if (writeYourNumber.value === randomNumber) {
+    getTrack.innerHTML = `Has ganado CAMPEONA!`;
+  } else if (writeYourNumber.value !== randomNumber) {
+    getTrack.innerHTML = `${writeYourNumber.value} está lejos del número premiado`;
+  }
+
+  const countDown = document.querySelector('.counter');
+
+  for (let i = 0; i < 100; i = i++); {
+    countDown.innerHTML = countDown + i;
+  }
 }
 
 button.addEventListener('click', probeYourLuck);
-
-
-
-
