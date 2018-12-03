6547862223a6bf21070997fd101aa4b7477a711b
aroatdlt <aroatato@gmx.com>
2018-12-03 17:46:56 +0100

    Try programming luck number

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const writeYourNumber = document.querySelector('#choose_number');
+const writeYourNumber = document.querySelector('.choose_number');
 const button = document.querySelector('.btn');
 const getTrack = document.querySelector('.feedback');
 
 const probeYourLuck = () => {
-    if (writeYourNumber.innerHTML === '12') {
-        getTrack.innerHTML = `Enhorabuena! ${writeYourNumber.innerHTML} es el número premiado!`;
+    if (writeYourNumber.value === 12) {
+        getTrack.innerHTML = `Enhorabuena! ${writeYourNumber.value} es el número premiado!`;
+    } else if (writeYourNumber.value > 20) {
+        getTrack.innerHTML = `Estás lejos del número premiado`;
+    } else if (writeYourNumber.value < 8) {
+        getTrack.innerHTML = `Estás lejos del número premiado`;
     }
 }
-
 
 button.addEventListener('click', probeYourLuck);
 
