d5a6057b49d879886d8b871f683edbb215ef0546
aroatdlt <aroatato@gmx.com>
2018-12-03 17:34:45 +0100

    Add const in js file

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('>> Ready :)');
+const writeYourNumber = document.querySelector('#choose_number');
+const button = document.querySelector('.btn');
+const getTrack = document.querySelector('.feedback');
+
+const probeYourLuck = () => {
+    if (writeYourNumber.innerHTML === '12') {
+        getTrack.innerHTML = `Enhorabuena! ${writeYourNumber.innerHTML} es el número premiado!`;
+    }
+}
+
+
+button.addEventListener('click', probeYourLuck);
+
+
+
+
