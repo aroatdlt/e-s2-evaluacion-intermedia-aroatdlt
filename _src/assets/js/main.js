14dd146b1b8e55a8b7d730e5125d1d16718fc0fd
aroatdlt <aroatato@gmx.com>
2018-12-03 18:16:38 +0100

    New changes in code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,15 +2,25 @@
 
 const writeYourNumber = document.querySelector('.choose_number');
 const button = document.querySelector('.btn');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+console.log('> ' + getRandomNumber(100));
+
+const randomNumber = getRandomNumber;
+
 const getTrack = document.querySelector('.feedback');
 
 const probeYourLuck = () => {
-    if (writeYourNumber.value === 12) {
-        getTrack.innerHTML = `Enhorabuena! ${writeYourNumber.value} es el número premiado!`;
-    } else if (writeYourNumber.value > 20) {
-        getTrack.innerHTML = `Estás lejos del número premiado`;
-    } else if (writeYourNumber.value < 8) {
-        getTrack.innerHTML = `Estás lejos del número premiado`;
+    
+    if (writeYourNumber.value === randomNumber) {
+        getTrack.innerHTML = `Has ganado CAMPEONA!`;
+    } else if (writeYourNumber.value > randomNumber) {
+        getTrack.innerHTML = `${writeYourNumber.value} está lejos del número premiado`;
+    } else if (writeYourNumber.value < randomNumber) {
+        getTrack.innerHTML = `${writeYourNumber.value} está lejos del número premiado`;
     }
 }
 
