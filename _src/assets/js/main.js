8cfd137269c30f2988699969c98be07954132603
aroatdlt <aroatato@gmx.com>
2018-12-06 11:53:15 +0100

    Fix all the things in js to a good performance

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,26 +2,34 @@
 
 const writeYourNumber = document.querySelector('.choose_number');
 const button = document.querySelector('.btn');
+const getTrack = document.querySelector('.feedback');
+const countUp = document.querySelector('.counter');
+let randomNumber;
+let counter = 0;
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log('> ' + getRandomNumber(100));
-
-const randomNumber = getRandomNumber;
-
-const getTrack = document.querySelector('.feedback');
-
-const probeYourLuck = () => {
-
-  if (writeYourNumber.value === randomNumber) {
-    getTrack.innerHTML = `Has ganado CAMPEONA!`;
-  } else if (writeYourNumber.value !== randomNumber) {
-    getTrack.innerHTML = `${writeYourNumber.value} está lejos del número premiado`;
-  }
-
-  const countDown = document.querySelector('.counter');
+function counterClick() {
+  countUp.innerHTML = counter;
 }
 
-button.addEventListener('click', probeYourLuck);
+randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+const handledprobeYourLuck = () => {
+  const inputElement = parseInt(writeYourNumber.value);
+  if (inputElement === randomNumber) {
+    getTrack.innerHTML = `HAS GANADO, CAMPEONA!`;
+  } else if (inputElement > randomNumber) {
+    getTrack.innerHTML = `Demasiado alto`;
+  } else if (inputElement < randomNumber) {
+    getTrack.innerHTML = `Demasiado bajo`;
+  }
+  counter = counter + 1;
+  counterClick();
+}
+
+button.addEventListener('click', handledprobeYourLuck);
+counterClick();
